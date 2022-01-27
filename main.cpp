51f3e76eb4b900341fc5ef881cdde63eb805e032
tidilia <68690142+tidilia@users.noreply.github.com>
2022-01-27 17:58:06 +0500

    laba02

--- main.cpp
+++ main.cpp
@@ -1,133 +1,57 @@
 #include <iostream>
-#include "memory"
+#include <string>
 using namespace std;
 
-class Base{
-
-    int val = 0;
+class GrandParent {
+protected:
+    string str = "GrandParent";
 public:
-    Base() {
-        val = 0;
-        cout << "Base()\n";
-    };
-
-    Base(Base *obj) {
-        setVal(obj->getVal());
-        cout << "Base(*obj)\n";
-    };
-
-    Base(Base &obj) {
-        setVal(obj.getVal());
-        cout << "Base(&obj)\n";
-    };
-
-
-    ~Base() {
-        cout << "~Base()\n";
+    virtual string get_str(){
+        return "GrandParent";
     }
-
-    int getVal() const;
-
-    void setVal(int val);;
-};
-
-class Desc: public Base{
-public:
-    Desc() {};
-
-    Desc(Desc *obj) {
-        setVal(obj->getVal());
-        cout << "Desc(*obj)\n";
-    };
-
-    Desc(Desc &obj) {
-        setVal(obj.getVal());
-        cout << "Desc(&obj)\n";
-    };
-
-    ~Desc() {
-        cout << "~Desc()";
+    void greeting(){
+        cout << "Hello, Granny!\n";
     }
 };
 
-void func1(Base obj) {
-    cout << "func1(obj). value = " << obj.getVal() << "\n";
-};
-
-void func2(Base *obj) {
-    cout << "func2(*obj). value = " << obj->getVal() << "\n";
-};
-
-void func3(Base &obj) {
-    cout << "func1(&obj). value = " << obj.getVal() << "\n";
-};
-
-
-
-Base* func1() {
-    cout << "func1\n";
-    Base obj;
-    obj.setVal(5);
-    return &obj;
-};
-
-Base* func2() {
-    cout << "func2\n";
-    Base *ptr = new Base();
-    return ptr;
-};
-
-Base& func3() {
-    cout << "func3\n";
-    Base *ptr = new Base();
-    return *ptr;
-};
-
-int Base::getVal() const {
-    return val;
-}
-
-void Base::setVal(int val) {
-    Base::val = val;
-}
-
-class A {
+class Parent: public GrandParent{
 public:
-    A() { cout << "A()" << endl; }
-    ~A() { cout << "~A()" << endl; }
+    virtual string get_str() override{
+        str = "Parent";
+        return str;
+    }
+    void greeting(){
+        cout << "Hello, Mom and Dad!\n";
+    }
 };
 
-class B : public A {
+class Child: public Parent{
 public:
-    B() { cout << "B()" << endl; }
-    ~B() { cout << "~B()" << endl; }
+    string get_str() override{
+        str = "Child";
+        return get_str();
+    }
+    void greeting(){
+        cout << "Hello, Child!\n";
+    }
 };
-
 
 int main() {
+    GrandParent Granny = GrandParent(); //создаем объект grandparent
+    Parent parent = Parent(); //создаем объект parent
+    GrandParent *parent_ptr = &parent; //помещаем объект parent в указатель родительского типа
 
-    A * pA = new B;
-    delete pA;
+    cout << "Функция greeting без ключевого слова virtual:\n";
+    cout << "Для объекта типа grandparent:\n";
+    Granny.greeting();
+    cout << "Для объекта типа parent:\n";
+    parent.greeting();
+    cout << "Для объекта типа parent в указателе типа grandparent:\n";
+    parent_ptr->greeting();
 
-    Base el1;
-    el1 = func1();
-    Base *el2 = func2();
-    Base *el3 = &func3();
-    Desc *d = new Desc();
-    delete d;
-
-    int *ptr1 = new int(5);
-    unique_ptr<int> up(new int(5));
-    unique_ptr<int> up2(ptr1);
-    //unique_ptr<int> up2(up); так нельзя так как это уникальные указатели
-    int *ptr2 = up2.get();
-    up2.release(); //только up2 станет пустым
-    //up2.reset() сотрет данные в целом и все указатели станут пустыми
-
-    shared_ptr<string> sharedPtr1(new string("hello"));
-    shared_ptr<string> sharedPtr2(sharedPtr1);
-
-
-    cout << "\n";
+    cout << "\nРабота virtual функции get_str:\n";
+    cout << "Для объекта типа grandparent:\n" << Granny.get_str() << "\n";
+    cout << "Для объекта типа parent:\n" << parent.get_str() << "\n";
+    cout << "Для объекта типа parent в указателе типа grandparent:\n" << parent_ptr->get_str();
     return 0;
 }
