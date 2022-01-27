107c4fd06e17d1f6b33005dbd3758c643bfb49bc
tidilia <68690142+tidilia@users.noreply.github.com>
2022-01-27 17:44:47 +0500

    laba04_1

--- Form1.cs
+++ Form1.cs
@@ -7,180 +7,134 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Timers;
 
-namespace laba01
+namespace laba04OOP
 {
     public partial class Form1 : Form
     {
-        public int counter = 1;
-        public int width = 20;
-        public int height = 350;
+        int id = 0;
+        int selected = 0;
+        List<MyCircle> circles = new List<MyCircle>();
+        List<MyCircle> selected_circles = new List<MyCircle>();
+        bool isCtrlPressed = false;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-
-        private void delB_Click(object sender, EventArgs e)
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            delBox.Text = "";
-            progressBar1.Value = 30;
-        }
-
-        private void OKnameB_Click(object sender, EventArgs e)
-        {
-            string greeting = "Hello, " + textBox1.Text;
-            greetBox.Text = greeting;
-            delBox.Text = greeting;
-            progressBar1.Value = 20;
-        }
-
-        private void greetBox_MouseHover(object sender, EventArgs e)
-        {
-            greetBox.ForeColor = Color.Green;
-        }
-
-        private void greetBox_MouseLeave(object sender, EventArgs e)
-        {
-            greetBox.ForeColor = Color.Black;
-        }
-
-        private void LanguageCBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            string new_t = LanguageCBox.Text + " is chosen";
-            LanguageL.Text = new_t;
-            progressBar1.Value = 40;
-        }
-
-        private void WorkCompleteChBox_CheckStateChanged(object sender, EventArgs e)
-        {
-            MessageBox.Show("Congratulations!");
-            progressBar1.Value = 60;
-        }
-
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-            if (radioButton1.CheckAlign == ContentAlignment.MiddleLeft)
+            if (e.KeyCode == Keys.ControlKey)
             {
-                radioButton1.CheckAlign = ContentAlignment.MiddleRight;
+                isCtrlPressed = true;
+                label1.Text = "true";
+            }
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ControlKey) isCtrlPressed = false;
+        }
+
+        private void PictureB_Paint(object sender, PaintEventArgs e)
+        {
+            for(int i = 0; i < circles.Count; ++i)
+            {
+                circles[i].draw(e);
+            }
+        }
+
+        private void PictureB_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) Circle_Creator(sender, e);
+            if (e.Button == MouseButtons.Right) Del_circles();
+        }
+
+        private void Del_circles()
+        {
+            for(int i = 0; i < selected_circles.Count; ++i)
+            {
+                for (int j = 0; j < circles.Count; ++j)
+                    if (selected_circles[i] == circles[j]) circles.RemoveAt(j);
+            }
+            selected_circles.Clear();
+            selected = 0;
+            Refresh();
+        }
+        private void Circle_Creator(object sender, MouseEventArgs e)
+        {
+            bool hit = false;
+            MyCircle new_circle;
+                for (int i = 0; i < circles.Count; ++i)
+                    if (circles[i].hittest(e.X, e.Y))
+                    {
+                        if (!isCtrlPressed)
+                            for (int j = 0; j < circles.Count; ++j)
+                            {
+                                circles[j].IsSelected = false;
+                                selected_circles.Clear();
+                                selected = 0;
+                            }
+                        sel(i);
+                        hit = true;
+                    }
+            if (!hit)
+            {
+                new_circle = new MyCircle(e.X, e.Y, id);
+                ++id;
+                if (circles.Count == 0)
+                {
+                    new_circle.IsSelected = true;
+                    selected_circles.Add(new_circle);
+                }
+                circles.Add(new_circle);
+            }
+
+
+            Refresh(); //ручной вызов PaintEvent
+
+        }
+        
+        private void sel(int i)
+        {
+            if (circles[i].IsSelected)
+            {
+                selected--;
+                selected_circles.Remove(circles[i]);
             }
             else
             {
-                radioButton1.CheckAlign = ContentAlignment.MiddleLeft;
+                selected++;
+                selected_circles.Add(circles[i]);
             }
-            progressBar1.Value = 50;
+            circles[i].IsSelected = !circles[i].IsSelected;
         }
 
-        private void UniversLBox_SelectedIndexChanged(object sender, EventArgs e)
+        /*private void GraphBox_RightButton(object sender, MouseEventArgs e)
         {
-            if(UniversLBox.Text == "USATU")
+            for (int i = 0; i < id; ++i)
             {
-                testL.Text = "Hello, bro!";
-            } else
-            {
-                testL.Text = "Test is failed :(";
+                if (cities[i].hittest(e.X, e.Y))
+                {
+                    if (!cities[i].IsSelected && selected < 2)
+                    {
+                        cities[i].IsSelected = true;
+                        selected++;
+                        selected_cities.Add(cities[i]);
+                    }
+                    else if (cities[i].IsSelected)
+                    {
+                        cities[i].IsSelected = false;
+                        if (selected_cities[0] == cities[i]) selected_cities.Remove(selected_cities[0]);
+                        else if (selected_cities[1] == cities[i]) selected_cities.Remove(selected_cities[1]);
+                        selected--;
+                    }
+
+                    map = new Map(cities, roads);
+                    Refresh();
+                }
             }
-            progressBar1.Value = 70;
-        }
+        }*/
 
-        private void colorP_Click(object sender, EventArgs e)
-        {
-            Random rnd = new Random();
-            int[] abc = new int[3];
-            for(int i = 0; i < 3; ++i)
-                abc[i] = rnd.Next(0, 254);
-            colorP.BackColor = Color.FromArgb(abc[0], abc[1], abc[2]);
-            progressBar1.Value = 80;
-        }
-
-        private void colorP_MouseHover(object sender, EventArgs e)
-        {
-            colorP.Size = new Size(70,70);
-        }
-
-        private void colorP_MouseLeave(object sender, EventArgs e)
-        {
-            colorP.Size = new Size(90,90);
-        }
-
-        private void textBox1_Validated(object sender, EventArgs e)
-        {
-            progressBar1.Value = 10;
-        }
-
-        private void progressBar1_Click(object sender, EventArgs e)
-        {
-            progressBar1.Value = 100;
-        }
-
-        private void newfbut_Click(object sender, EventArgs e)
-        {
-            Button button = new Button();
-            button.Size = new Size(30, 15);
-            button.Name = "but" + counter;
-            button.Location = MousePosition;
-            this.Controls.Add(button);
-        }
-
-
-        private void newbbut_Click(object sender, EventArgs e)
-        {
-
-            Button button = new Button();
-            button.Size = new Size(50, 25);
-            button.Name = "but" + counter;
-            button.Text = button.Name;
-            button.Location = new Point(width, height);
-            button.Click += new System.EventHandler(this.remove_Click);
-            width += 60;
-            if (width > 315)
-            {
-                width = 20;
-                height += 35;
-            }
-            this.Controls.Add(button);
-            counter++;
-        }
-
-        private void remove_Click(object sender, EventArgs e)
-        {
-            this.Controls.Remove(sender as Button);
-        }
-
-        private void birthMaskTB_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
-        {
-            birthMaskTB.ForeColor = Color.Red;
-        }
-
-        private void birthMaskTB_Validated(object sender, EventArgs e)
-        {
-            birthMaskTB.ForeColor = Color.Green;
-        }
-
-        private void monthCalendar1_DateSelected(object sender, DateRangeEventArgs e)
-        {
-            /*DateTime examDate = new DateTime(2022, 1, 19);
-            if (e.Equals(examDate)) label2.Text = "exam date";*/
-            label2.Text = "Date Selected: Start = " + e.Start.ToShortDateString() +
-                " : End = " + e.End.ToShortDateString();
-        }
-
-        int i = 0;
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            i++;
-            timerLabel.Text = i.ToString();
-        }
-
-        private void TimerButton_Click(object sender, EventArgs e)
-        {
-            timer1.Enabled = !timer1.Enabled;
-        }
-
-        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            LanguageCBox.SelectedIndex = tabControl1.SelectedIndex;
-        }
     }
 }
