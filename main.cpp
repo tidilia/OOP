9f56f889f61e7ad50c2c63a45d22055732f68ea8
tidilia <68690142+tidilia@users.noreply.github.com>
2022-01-27 17:57:01 +0500

    laba05_2

--- main.cpp
+++ main.cpp
@@ -1,157 +1,133 @@
 #include <iostream>
-#include <typeinfo>
+#include "memory"
 using namespace std;
 
+class Base{
 
-class Base {
-protected:
-    void second_method(){
-        cout << "Base::second method\n";
-    }
+    int val = 0;
 public:
-    virtual string classname(){
-        return "Base";
+    Base() {
+        val = 0;
+        cout << "Base()\n";
+    };
+
+    Base(Base *obj) {
+        setVal(obj->getVal());
+        cout << "Base(*obj)\n";
+    };
+
+    Base(Base &obj) {
+        setVal(obj.getVal());
+        cout << "Base(&obj)\n";
+    };
+
+
+    ~Base() {
+        cout << "~Base()\n";
     }
-    virtual bool isA(string classname){
-        if(classname == "Base") return true;
-        else return false;
-    }
-    void first_method(){
-        cout << "first_method -> ";
-        second_method();
-    }
-    virtual void NameOf(){ // Virtual function.
-        cout << "Base::NameOf\n";
-    }
-    void InvokingClass(){  // Nonvirtual function.
-        cout << "Invoked by Base\n";
-    }
-    virtual ~Base(){
-        cout << "Base's destructor\n";
+
+    int getVal() const;
+
+    void setVal(int val);;
+};
+
+class Desc: public Base{
+public:
+    Desc() {};
+
+    Desc(Desc *obj) {
+        setVal(obj->getVal());
+        cout << "Desc(*obj)\n";
+    };
+
+    Desc(Desc &obj) {
+        setVal(obj.getVal());
+        cout << "Desc(&obj)\n";
+    };
+
+    ~Desc() {
+        cout << "~Desc()";
     }
 };
 
-class Derived : public Base {
-protected:
-    void second_method(){
-        cout << "Derived::second_method\n";
-    }
-public:
-    string classname() override;
-    bool isA(string classname) override;
-    void NameOf() override;   // Virtual function.
-    void InvokingClass();   // Nonvirtual function.
-    ~Derived() override;
+void func1(Base obj) {
+    cout << "func1(obj). value = " << obj.getVal() << "\n";
+};
+
+void func2(Base *obj) {
+    cout << "func2(*obj). value = " << obj->getVal() << "\n";
+};
+
+void func3(Base &obj) {
+    cout << "func1(&obj). value = " << obj.getVal() << "\n";
 };
 
 
-string Derived::classname() {
-    return "Derived";
-}
 
-bool Derived::isA(string classname) {
-    return((classname == "Derived") || Base::isA(classname));
-}
-
-void Derived::NameOf() {
-    cout << "Derived::NameOf\n";
-}
-
-void Derived::InvokingClass() {
-    cout << "Invoked by Derived\n";
-}
-
-Derived::~Derived() noexcept {
-    cout << "Derived's destructor\n";
-}
-
-class Point {
-protected:
-    int x;
-    int y;
-public:
-    Point() {
-        x = 0;
-        y = 0;
-        printf("Point()\n");
-    }
-    virtual void Draw(){
-        cout << x << " " << y;
-    }
-    ~Point(){
-        printf("x = %d, y = %d\n", x, y);
-        printf("~Point()\n");
-    }
+Base* func1() {
+    cout << "func1\n";
+    Base obj;
+    obj.setVal(5);
+    return &obj;
 };
 
-class ColoredPoint: public Point{
-protected:
-    int Color;
-public:
-    void Draw() override{
-        cout << x << " " << y << " " << Color;
-    }
+Base* func2() {
+    cout << "func2\n";
+    Base *ptr = new Base();
+    return ptr;
 };
 
-class C: public Base{};
-class D: public C{};
-class Other{ virtual void fooo(){};};
+Base& func3() {
+    cout << "func3\n";
+    Base *ptr = new Base();
+    return *ptr;
+};
+
+int Base::getVal() const {
+    return val;
+}
+
+void Base::setVal(int val) {
+    Base::val = val;
+}
+
+class A {
+public:
+    A() { cout << "A()" << endl; }
+    ~A() { cout << "~A()" << endl; }
+};
+
+class B : public A {
+public:
+    B() { cout << "B()" << endl; }
+    ~B() { cout << "~B()" << endl; }
+};
 
 
 int main() {
-    // Declare an object of type Derived.
-    Derived aDerived;
 
-    cout<<"\nПроверки на принадлежность типам\n" << "Принадлежит ли объект aDerived классу Base?\n" << "Проверка classname() -> ";
-    if (aDerived.classname() == "Base") cout << "Да.\n";
-    else cout << "Нет.\n";
-    cout<< "Принадлежит ли объект aDerived классу Base?\n" << "Проверка isA() -> ";
-    if (aDerived.isA("Base")) cout << "Да.\n";
-    else cout << "Нет.\n";
-    cout << "Принадлежит ли объект aDerived классу Derived?\n" << "Проверка isA() -> ";
-    if (aDerived.isA("Derived")) cout << "Да.\n";
-    else cout << "Нет.\n";
+    A * pA = new B;
+    delete pA;
 
-    // Declare two pointers, one of type Derived * and the other
-    //  of type Base *, and initialize them to point to aDerived.
-    Derived *pDerived = &aDerived;
-    Base    *pBase    = &aDerived;
+    Base el1;
+    el1 = func1();
+    Base *el2 = func2();
+    Base *el3 = &func3();
+    Desc *d = new Desc();
+    delete d;
 
-    // Call the functions.
-    cout << "\nДемонстрация работы виртуальных и невиртуальных методов с помощью указателей разного типа\n";
-    pBase->NameOf();           // Call virtual function.
-    pBase->InvokingClass();    // Call nonvirtual function.
-    pDerived->NameOf();        // Call virtual function.
-    pDerived->InvokingClass(); // Call nonvirtual function.
-    aDerived.first_method();
+    int *ptr1 = new int(5);
+    unique_ptr<int> up(new int(5));
+    unique_ptr<int> up2(ptr1);
+    //unique_ptr<int> up2(up); так нельзя так как это уникальные указатели
+    int *ptr2 = up2.get();
+    up2.release(); //только up2 станет пустым
+    //up2.reset() сотрет данные в целом и все указатели станут пустыми
 
-    cout << "\nВиртуальный деструктор для Derived\n";
-    {
-        Derived newDerived;
-    }
+    shared_ptr<string> sharedPtr1(new string("hello"));
+    shared_ptr<string> sharedPtr2(sharedPtr1);
 
 
-
-    cout << "\nБезопасное приведение типов с помощью dynamic_cast\n";
-    D *pd = new D;
-    C *pc = dynamic_cast<C*>(pd);
-    cout << typeid(pc).name() << endl; //Type: C
-    Base *pb = dynamic_cast<Base*>(pd);
-    cout << typeid(pb).name() << "\n"; //Type: Base
-
-    //Нельзя производить преобразование вниз по иерархии если базовый класс не содержит virtual
-    C* pc2 = dynamic_cast<C*>(pb);
-    cout << typeid(pc2).name() << endl; //Type: C
-
-    //Проверка класса во время выполнения
-    Other *otherptr = new Other();
-    if(dynamic_cast<Base*>(otherptr))
-        cout << "Base\n";
-    else if(dynamic_cast<Other*>(otherptr))
-        cout << "Other\n";
-
-    ColoredPoint *p = new ColoredPoint;
-    Point *p2 = dynamic_cast<Point*>(p);
-    p2->Draw();
-
+    cout << "\n";
+    return 0;
 }
